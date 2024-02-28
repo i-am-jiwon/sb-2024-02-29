c42ec57c0d47c213cfc41abc168798c140f846d4
i-am-jiwon <dlark1128@gmail.com>
2024-02-29 01:42:59 +0900

    work

---
+++ src/test/java/com/ll/sb20240229/Sb20240229ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20240229;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20240229ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
