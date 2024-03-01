77417958054da376d3f0d0ae9d03c1441c7a48bc
i-am-jiwon <dlark1128@gmail.com>
2024-03-01 23:53:49 +0900

    work

--- src/main/java/com/ll/sb20240229/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb20240229/domain/home/home/controller/HomeController.java
@@ -11,6 +11,6 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain(){
-        return "Hello, world222";
+        return "Hello, world2223";
     }
 }
