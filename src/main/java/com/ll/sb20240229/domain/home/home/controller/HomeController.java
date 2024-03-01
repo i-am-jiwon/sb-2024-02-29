95d90e4113b10059f0b6cb53763941ee550d5ada
i-am-jiwon <dlark1128@gmail.com>
2024-03-02 00:50:38 +0900

    gha

--- src/main/java/com/ll/sb20240229/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb20240229/domain/home/home/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.ll.sb20240229.domain.home.home.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    @Value("${custom.jwt.secretKey}")
+    private String jwtSecretKey;
     @GetMapping("/")
     @ResponseBody
     public String showMain(){
         return "Hello, world2223";
+    }
+
+    @GetMapping("/jwtSecretKey")
+    @ResponseBody
+    public String showJwtSecretKey(){
+        return jwtSecretKey;
     }
 }
